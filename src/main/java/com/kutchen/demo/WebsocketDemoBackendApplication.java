6b08719a86259d6eabc0c7cf05ca1a4a2802da72
Peter <pgkutc@gmail.com>
2021-01-21 08:21:18 -0500

    websocket backend

---
+++ src/main/java/com/kutchen/demo/WebsocketDemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kutchen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketDemoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketDemoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kutchen/demo/WebsocketDemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutchen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketDemoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
